1f0f813c376615cc4f4d94a278ec7e117898e82b
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-28 16:37:42 -0300

    Update Aluno.java

--- AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/models/Aluno.java
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/models/Aluno.java
@@ -2,14 +2,35 @@ package br.com.alura.example.agenda.models;
 
 import android.support.annotation.NonNull;
 
-public class Aluno {
-    private final String nome;
-    private final String telefone;
-    private final String email;
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+    private Integer id = 0;
+    private String nome;
+    private String telefone;
+    private String email;
+
 
     public Aluno(String nome, String telefone, String email) {
+
         this.nome = nome;
         this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Aluno() {
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 
@@ -17,5 +38,29 @@ public class Aluno {
     @Override
     public String toString() {
         return this.nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setid(Integer id) {
+        this.id = id;
+    }
+
+    public boolean temIdValido() {
+        return this.id>0;
     }
 }
