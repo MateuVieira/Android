77c83129e0dc25639428e3927614f6ecb8de3046
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-28 01:00:16 -0300

    Create FormularioAlunoActivity.java

---
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,15 @@
+package br.com.alura.example.agenda.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.alura.example.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
