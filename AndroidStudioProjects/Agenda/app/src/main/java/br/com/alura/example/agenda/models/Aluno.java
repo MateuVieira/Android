f41af87d5df77b754ff8b3c720991985b9442ac4
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-28 10:22:36 -0300

    Create Aluno.java

---
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/models/Aluno.java
@@ -0,0 +1,21 @@
+package br.com.alura.example.agenda.models;
+
+import android.support.annotation.NonNull;
+
+public class Aluno {
+    private final String nome;
+    private final String telefone;
+    private final String email;
+
+    public Aluno(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+}
