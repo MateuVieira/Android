d50e7659d760341cceaecfabdcd155a0bca999d8
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-28 17:06:33 -0300

    Create ConstantesActivities.java

---
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/ui/activity/ConstantesActivities.java
@@ -0,0 +1,8 @@
+package br.com.alura.example.agenda.ui.activity;
+
+public interface ConstantesActivities {
+
+    public static final String TITLE_APPBAR_EDITA = "Edita aluno";
+    public static final String TITLE_APPBAR_NOVO_ALUNO = "Novo aluno";
+    public static final String CHAVE_ALUNO = "aluno";
+}
