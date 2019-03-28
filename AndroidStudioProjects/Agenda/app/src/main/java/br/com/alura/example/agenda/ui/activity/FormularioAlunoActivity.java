b73e31c14595d46780d193aaedca19ad3b93ec57
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-28 10:22:18 -0300

    Update FormularioAlunoActivity.java

--- AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,9 +1,16 @@
 package br.com.alura.example.agenda.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.alura.example.agenda.R;
+import br.com.alura.example.agenda.dao.AlunoDAO;
+import br.com.alura.example.agenda.models.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -11,5 +18,29 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final AlunoDAO dao = new AlunoDAO();
+
+        final EditText campoNome = findViewById(R.id.activit_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activit_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activit_formulario_aluno_email);
+
+        Button buttonSalvar = findViewById(R.id.activit_formulario_button_salvar);
+        buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno aluno = new Aluno(nome, telefone, email);
+
+                dao.salva(aluno);
+
+                startActivity(new Intent(FormularioAlunoActivity.this,
+                                            ListaAlunosActivity.class));
+            }
+        });
     }
 }
