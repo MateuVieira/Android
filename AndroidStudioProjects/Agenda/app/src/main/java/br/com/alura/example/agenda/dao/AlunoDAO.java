e72d9d85a1ce89514ee1436be525d4323cd5a7a8
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-28 16:37:37 -0300

    Update AlunoDAO.java

--- AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/dao/AlunoDAO.java
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/dao/AlunoDAO.java
@@ -1,5 +1,7 @@
 package br.com.alura.example.agenda.dao;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,12 +11,34 @@ import br.com.alura.example.agenda.models.Aluno;
 public class AlunoDAO {
 
     private final static List<Aluno> alunos = new LinkedList<>();
+    private static Integer contadorId = 1;
 
     public void salva(Aluno aluno) {
+
+        aluno.setid(contadorId);
         alunos.add(aluno);
+        contadorId++;
     }
 
     public List<Aluno> todos() {
         return new ArrayList<>(alunos);
+    }
+
+    public void edita(Aluno aluno){
+
+        Aluno alunoEncontrado = null;
+        for (Aluno a: alunos) {
+
+            Boolean validation = a.getId().intValue() == aluno.getId().intValue();
+            if(validation){
+                alunoEncontrado = a;
+                break;
+            }
+        }
+
+        if(alunoEncontrado != null){
+            int posicao = alunos.indexOf(alunoEncontrado);
+            alunos.set(posicao, aluno);
+        }
     }
 }
