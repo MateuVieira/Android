a9c81b3433979cc5e4202c2f98859a398503bc78
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-28 10:39:30 -0300

    Update ListaAlunosActivity.java

--- AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/ui/activity/ListaAlunosActivity.java
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -2,6 +2,7 @@ package br.com.alura.example.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -23,16 +24,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
 
-        AlunoDAO dao = new AlunoDAO();
-
-        ListView listaDeAlunos = findViewById(R.id.activity_lista_de_alunos_listwiew);
-
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                dao.todos()));
-
-        Button fabAdd = findViewById(R.id.activity_lista_alunos_fab_Add);
+        FloatingActionButton fabAdd = findViewById(R.id.activity_lista_alunos_fab_Add);
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,5 +33,19 @@ public class ListaAlunosActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AlunoDAO dao = new AlunoDAO();
+
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_de_alunos_listwiew);
+
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dao.todos()));
     }
 }
