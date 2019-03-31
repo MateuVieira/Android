6019ad3faa34720a54b57ad16ac7ee18a7dacdb2
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-31 18:11:01 -0300

    Update ListaPacotesActivity.java

--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ListaPacotesActivity.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ListaPacotesActivity.java
@@ -13,11 +13,17 @@ import br.com.alura.example.aluraviagem.models.Pacote;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
+    public static final String TITLE_APPBAR = "Pacotes";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        setTitle(TITLE_APPBAR);
+        configuraLista();
+    }
 
+    private void configuraLista() {
         List<Pacote> pacotes = new PacoteDao().lista();
         ListView listaPacotes = findViewById(R.id.lista_pacotes_listview);
         listaPacotes.setAdapter(new ListaPacotesAdapter(pacotes, ListaPacotesActivity.this));
