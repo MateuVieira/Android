987e7447fa7f3a91a72d4d62d9312ed1bafd0d6b
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-31 13:05:22 -0300

    Update ListaPacotesActivity.java

--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ListaPacotesActivity.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ListaPacotesActivity.java
@@ -2,8 +2,11 @@ package br.com.alura.example.aluraviagem.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import br.com.alura.example.aluraviagem.R;
+import br.com.alura.example.aluraviagem.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -11,5 +14,8 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+        ListView listaPacotes = findViewById(R.id.lista_pacotes_listview);
+        listaPacotes.setAdapter(new ListaPacotesAdapter());
     }
 }
