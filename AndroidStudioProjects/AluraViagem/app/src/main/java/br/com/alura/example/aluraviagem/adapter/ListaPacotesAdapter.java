22c16e969484c8f93f97482680100e550a014386
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-31 18:05:22 -0300

    Refectoring Adapter

--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/adapter/ListaPacotesAdapter.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/adapter/ListaPacotesAdapter.java
@@ -1,13 +1,21 @@
 package br.com.alura.example.aluraviagem.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.alura.example.aluraviagem.R;
 import br.com.alura.example.aluraviagem.models.Pacote;
@@ -43,8 +51,47 @@ public class ListaPacotesAdapter extends BaseAdapter {
 
         View viewCriada = LayoutInflater.from(contex)
                             .inflate(R.layout.item_pacote, parent, false);
+        Pacote pacote = pacotes.get(position);
 
+        TextView local = viewCriada.findViewById(R.id.item_pacote_text_local);
+        local.setText(pacote.getLocal());
+
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_image_local);
+        imagem.setImageDrawable(getDrawableImage(pacote));
+
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_text_dias);
+        dias.setText(diasEmTexto(pacote));
+
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_text_valor);
+        preco.setText(precoFormatacaoBrasileira(pacote));
 
         return viewCriada;
+    }
+
+    private Drawable getDrawableImage(Pacote pacote) {
+        Resources resources = contex.getResources();
+        int idDoDrawable = resources.getIdentifier(pacote.getImagem(),
+                "drawable",
+                contex.getPackageName());
+        return resources.getDrawable(idDoDrawable);
+    }
+
+    private String precoFormatacaoBrasileira(Pacote pacote) {
+        NumberFormat formatoBrasileiro = DecimalFormat.getCurrencyInstance(
+                                            new Locale("pt", "br"));
+        BigDecimal precoPacote = pacote.getPreco();
+        return formatoBrasileiro.format(precoPacote).replace("R$", "R$ ");
+    }
+
+    private String diasEmTexto(Pacote pacote) {
+        int quantidaDeDias = pacote.getDias();
+        String diasTexto = "";
+
+        if(quantidaDeDias > 1){
+            diasTexto = quantidaDeDias + " dias";
+        } else {
+            diasTexto = quantidaDeDias + " dia";
+        }
+        return diasTexto;
     }
 }
--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/dao/PacoteDao.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/dao/PacoteDao.java
@@ -15,7 +15,7 @@ public class PacoteDao {
                 new Pacote("Belo Horizonte", "belo_horizonte_mg", 3, new BigDecimal(421.50)),
                 new Pacote("Recife", "recife_pe", 4, new BigDecimal(754.20)),
                 new Pacote("Rio de Janeiro", "rio_de_janeiro_rj", 6, new BigDecimal(532.55)),
-                new Pacote("Salvador", "salvador_sp", 5, new BigDecimal(899.99)),
+                new Pacote("Salvador", "salvador_ba", 5, new BigDecimal(899.99)),
                 new Pacote("Foz do Iguaçu", "foz_do_iguacu_pr", 1, new BigDecimal(289.90))
         ));
         return pacotes;
