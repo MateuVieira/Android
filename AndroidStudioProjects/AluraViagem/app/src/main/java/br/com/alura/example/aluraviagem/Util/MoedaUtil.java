7f238a8f7d0003f321d9ae2e2e572c6771521490
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-31 18:38:00 -0300

    Update MoedaUtil.java

--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/Util/MoedaUtil.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/Util/MoedaUtil.java
@@ -7,10 +7,15 @@ import java.util.Locale;
 
 public class MoedaUtil {
 
+    public static final String PT = "pt";
+    public static final String BR = "br";
+    public static final String $ = "R$";
+    public static final String $_ = "R$ ";
+
     public static String precoFormatacaoBrasileira(BigDecimal preco) {
         NumberFormat formatoBrasileiro = DecimalFormat.getCurrencyInstance(
-                new Locale("pt", "br"));
+                new Locale(PT, BR));
         BigDecimal precoPacote = preco;
-        return formatoBrasileiro.format(precoPacote).replace("R$", "R$ ");
+        return formatoBrasileiro.format(precoPacote).replace($, $_);
     }
 }
