b1fa8eb1e35d74f8acae43eaf4eca9039092c77b
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-01 17:17:58 -0300

    Update ListaPacotesAdapter.java

--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/adapter/ListaPacotesAdapter.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/adapter/ListaPacotesAdapter.java
@@ -8,12 +8,15 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import br.com.alura.example.aluraviagem.R;
 import br.com.alura.example.aluraviagem.Util.DiasUtil;
+import br.com.alura.example.aluraviagem.Util.FormatadorDeDataUtil;
 import br.com.alura.example.aluraviagem.Util.ImagemUtil;
 import br.com.alura.example.aluraviagem.Util.MoedaUtil;
+import br.com.alura.example.aluraviagem.Util.PreencheUtil;
 import br.com.alura.example.aluraviagem.models.Pacote;
 
 public class ListaPacotesAdapter extends BaseAdapter {
@@ -47,41 +50,16 @@ public class ListaPacotesAdapter extends BaseAdapter {
 
         View viewCriada = LayoutInflater.from(contex)
                             .inflate(R.layout.item_pacote, parent, false);
-
         Pacote pacote = pacotes.get(position);
-
         preenchendoListView(viewCriada, pacote);
-
         return viewCriada;
     }
 
     private void preenchendoListView(View viewCriada, Pacote pacote) {
-        preencheLocal(viewCriada, pacote);
-        preencheImagem(viewCriada, pacote);
-        preencheDias(viewCriada, pacote);
-        preenchePreco(viewCriada, pacote);
+        PreencheUtil.preencheLocal(viewCriada, pacote.getLocal(), R.id.item_pacote_text_local);
+        PreencheUtil.preencheImagem(viewCriada, pacote.getImagem(), R.id.item_pacote_image_local, contex);
+        PreencheUtil.preencheDia(viewCriada, pacote.getDias(), R.id.item_pacote_text_dias);
+        PreencheUtil.preenchePreco(viewCriada, pacote.getPreco(), R.id.item_pacote_text_valor);
     }
-
-    private void preenchePreco(View viewCriada, Pacote pacote) {
-        TextView preco = viewCriada.findViewById(R.id.item_pacote_text_valor);
-        preco.setText(MoedaUtil.precoFormatacaoBrasileira(pacote.getPreco()));
-    }
-
-    private void preencheDias(View viewCriada, Pacote pacote) {
-        TextView dias = viewCriada.findViewById(R.id.item_pacote_text_dias);
-        dias.setText(DiasUtil.diasEmTexto(pacote.getDias()));
-    }
-
-    private void preencheImagem(View viewCriada, Pacote pacote) {
-        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_image_local);
-        imagem.setImageDrawable(ImagemUtil.getDrawableImage(pacote.getImagem(), contex));
-    }
-
-    private void preencheLocal(View viewCriada, Pacote pacote) {
-        TextView local = viewCriada.findViewById(R.id.item_pacote_text_local);
-        local.setText(pacote.getLocal());
-    }
-
-
 
 }
