c3e8a4a2aec96af81fc3d0ca7130ef4753a2cd26
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-01 17:18:09 -0300

    Create ResumoCompraActivity.java

---
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ResumoCompraActivity.java
@@ -0,0 +1,58 @@
+package br.com.alura.example.aluraviagem.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+
+import br.com.alura.example.aluraviagem.R;
+import br.com.alura.example.aluraviagem.Util.FormatadorDeDataUtil;
+import br.com.alura.example.aluraviagem.Util.ImagemUtil;
+import br.com.alura.example.aluraviagem.Util.MoedaUtil;
+import br.com.alura.example.aluraviagem.models.Pacote;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    public static final String TITLE_APPBAR_RESUMO = "Resumo da Compra";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+        setTitle(TITLE_APPBAR_RESUMO);
+
+        Pacote pacote = new Pacote("São Paulo",
+                "sao_paulo_sp",
+                2,
+                new BigDecimal(243.99));
+
+        preencheImagem(pacote.getImagem(), R.id.resumo_compra_image);
+        preencheLocal(pacote.getLocal(), R.id.resumo_compra_text_local);
+        preencheData(pacote.getDias() , R.id.resumo_compra_text_data);
+        preenchePreco(pacote.getPreco(), R.id.resumo_compra_text_preco);
+
+
+    }
+
+    private void preencheData(int dia, int id) {
+        TextView pacoteData = findViewById(id);
+        pacoteData.setText(FormatadorDeDataUtil.formataData(dia));
+    }
+
+    private void preenchePreco(BigDecimal preco, int id) {
+        TextView pacotePreco = findViewById(id);
+        pacotePreco.setText(MoedaUtil.precoFormatacaoBrasileira(preco));
+    }
+
+    private void preencheLocal(String local, int id) {
+        TextView pacoteLocal = findViewById(id);
+        pacoteLocal.setText(local);
+    }
+
+    private void preencheImagem(String image, int id) {
+        ImageView pacoteImage = findViewById(id);
+        pacoteImage.setImageDrawable(ImagemUtil.getDrawableImage(image, this));
+    }
+}
