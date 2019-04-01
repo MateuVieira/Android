2c30af34c763ea224bc451080709fb003c15c373
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-03-31 23:48:40 -0300

    Create ResumoPacoteActivity.java

---
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,15 @@
+package br.com.alura.example.aluraviagem.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.alura.example.aluraviagem.R;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+    }
+}
