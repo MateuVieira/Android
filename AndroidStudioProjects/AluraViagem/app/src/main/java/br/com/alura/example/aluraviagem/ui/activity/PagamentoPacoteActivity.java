88ba3a897f964ca2efc1b8ae4a4f39038f9fc3c6
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-01 22:38:15 -0300

    Update PagamentoPacoteActivity.java

--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/PagamentoPacoteActivity.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/PagamentoPacoteActivity.java
@@ -3,6 +3,8 @@ package br.com.alura.example.aluraviagem.ui.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
@@ -11,9 +13,12 @@ import br.com.alura.example.aluraviagem.R;
 import br.com.alura.example.aluraviagem.Util.MoedaUtil;
 import br.com.alura.example.aluraviagem.models.Pacote;
 
+import static br.com.alura.example.aluraviagem.ui.activity.PacoteActivityConstantes.CHAVE_PACOTE;
+import static br.com.alura.example.aluraviagem.ui.activity.PacoteActivityConstantes.TITLE_APPBAR_PAGAMENTO;
+
 public class PagamentoPacoteActivity extends AppCompatActivity {
 
-    public static final String TITLE_APPBAR_PAGAMENTO = "Pagamento";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +26,31 @@ public class PagamentoPacoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pagamento_pacote);
         setTitle(TITLE_APPBAR_PAGAMENTO);
 
+        Intent intent = getIntent();
+        if ( intent.hasExtra(CHAVE_PACOTE)) {
 
-        Pacote pacote = new Pacote("São Paulo",
-                            "sao_paulo_sp",
-                            2,
-                            new BigDecimal(243.99));
+            final Pacote pacote = (Pacote) intent.getSerializableExtra(CHAVE_PACOTE);
+            preeenchePreco(pacote);
+            vaiParaResumoCompra(pacote);
+        }
+    }
 
-        preeenchePreco(pacote);
+    private void vaiParaResumoCompra(final Pacote pacote) {
+        Button botaoFinalizaCompra = findViewById(R.id.pagamento_pacote_button_finalizar_compra);
+        botaoFinalizaCompra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PagamentoPacoteActivity.this,
+                                            ResumoCompraActivity.class);
+                intent.putExtra(CHAVE_PACOTE, pacote);
+                startActivity(intent);
+            }
+        });
     }
 
     private void preeenchePreco(Pacote pacote) {
         TextView preco = findViewById(R.id.pagamento_pacote_text_preco);
         preco.setText(MoedaUtil.precoFormatacaoBrasileira(pacote.getPreco()));
-
-        Intent intent = new Intent(this, ResumoCompraActivity.class);
-        startActivity(intent);
     }
 
 
