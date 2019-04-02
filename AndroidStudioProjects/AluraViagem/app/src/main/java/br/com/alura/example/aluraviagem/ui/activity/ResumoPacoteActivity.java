8798c499503f29df0ae7f5a79fef3aa427f17ef6
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-01 22:38:28 -0300

    Update ResumoPacoteActivity.java

--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ResumoPacoteActivity.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ResumoPacoteActivity.java
@@ -3,6 +3,8 @@ package br.com.alura.example.aluraviagem.ui.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,30 +17,50 @@ import br.com.alura.example.aluraviagem.Util.ImagemUtil;
 import br.com.alura.example.aluraviagem.Util.MoedaUtil;
 import br.com.alura.example.aluraviagem.models.Pacote;
 
+import static br.com.alura.example.aluraviagem.ui.activity.PacoteActivityConstantes.CHAVE_PACOTE;
+import static br.com.alura.example.aluraviagem.ui.activity.PacoteActivityConstantes.TITLE_APPBAR_RESUMO_PACOTE;
+
 public class ResumoPacoteActivity extends AppCompatActivity {
 
-    public static final String TITLE_APPBAR_RESUMO = "Resumo do Pacote";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
-        setTitle(TITLE_APPBAR_RESUMO);
+        setTitle(TITLE_APPBAR_RESUMO_PACOTE);
+        carregaPacoteRecebido();
+    }
 
-        Pacote pacote = new Pacote("São Paulo",
-                    "sao_paulo_sp",
-                    2,
-                    new BigDecimal(243.99));
+    private void carregaPacoteRecebido() {
+        Intent intent = getIntent();
+        if ( intent.hasExtra(CHAVE_PACOTE)) {
 
+            final Pacote pacote = (Pacote) intent.getSerializableExtra(CHAVE_PACOTE);
+            preecheCampos(pacote);
+            vaiParaPagamentoPacote(pacote);
+        }
+    }
+
+    private void vaiParaPagamentoPacote(final Pacote pacote) {
+        Button botaoRealizaPagamento = findViewById(R.id.resumo_pacote_button_comprar);
+        botaoRealizaPagamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResumoPacoteActivity.this,
+                        PagamentoPacoteActivity.class);
+                intent.putExtra(CHAVE_PACOTE, pacote);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void preecheCampos(Pacote pacote) {
         preencheImagem(pacote.getImagem(), R.id.resumo_pacote_imagem);
         preencheLocal(pacote.getLocal(), R.id.resumo_pacote_text_local);
         preencheDia(pacote.getDias(), R.id.resumo_pacote_text_dias);
         preenchePreco(pacote.getPreco(), R.id.resumo_pacote_text_preco);
         preencheData(pacote.getDias(), R.id.resumo_pacote_text_data);
-
-        Intent intent = new Intent(this, PagamentoPacoteActivity.class);
-        startActivity(intent);
-
     }
 
     private void preencheData(int dia, int id) {
