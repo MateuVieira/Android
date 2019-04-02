4d15ef056a273571742388ca0c7a11dd82d4647f
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-01 22:37:58 -0300

    Update ListaPacotesActivity.java

--- AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ListaPacotesActivity.java
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/ListaPacotesActivity.java
@@ -3,6 +3,8 @@ package br.com.alura.example.aluraviagem.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
@@ -12,9 +14,12 @@ import br.com.alura.example.aluraviagem.adapter.ListaPacotesAdapter;
 import br.com.alura.example.aluraviagem.dao.PacoteDao;
 import br.com.alura.example.aluraviagem.models.Pacote;
 
+import static br.com.alura.example.aluraviagem.ui.activity.PacoteActivityConstantes.CHAVE_PACOTE;
+import static br.com.alura.example.aluraviagem.ui.activity.PacoteActivityConstantes.TITLE_APPBAR;
+
 public class ListaPacotesActivity extends AppCompatActivity {
 
-    public static final String TITLE_APPBAR = "Pacotes";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +27,24 @@ public class ListaPacotesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_pacotes);
         setTitle(TITLE_APPBAR);
         configuraLista();
-
-        Intent intent = new Intent(this, ResumoPacoteActivity.class);
-        startActivity(intent);
     }
 
     private void configuraLista() {
-        List<Pacote> pacotes = new PacoteDao().lista();
+        final List<Pacote> pacotes = new PacoteDao().lista();
         ListView listaPacotes = findViewById(R.id.lista_pacotes_listview);
         listaPacotes.setAdapter(new ListaPacotesAdapter(pacotes, ListaPacotesActivity.this));
+        listaPacotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                vaiParaResumoPacote(position, pacotes);
+            }
+        });
+    }
+
+    private void vaiParaResumoPacote(int position, List<Pacote> pacotes) {
+        Intent intent = new Intent(ListaPacotesActivity.this,
+                                                ResumoPacoteActivity.class);
+        intent.putExtra(CHAVE_PACOTE, pacotes.get(position));
+        startActivity(intent);
     }
 }
