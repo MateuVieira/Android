33c25bcc939508fb068b95c92f63f7db9eb87b47
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2019-04-01 22:38:02 -0300

    Create PacoteActivityConstantes.java

---
+++ AndroidStudioProjects/AluraViagem/app/src/main/java/br/com/alura/example/aluraviagem/ui/activity/PacoteActivityConstantes.java
@@ -0,0 +1,10 @@
+package br.com.alura.example.aluraviagem.ui.activity;
+
+public interface PacoteActivityConstantes {
+
+    String TITLE_APPBAR_RESUMO_PACOTE = "Resumo do Pacote";
+    String CHAVE_PACOTE = "pacote";
+    String TITLE_APPBAR = "Pacotes";
+    String TITLE_APPBAR_PAGAMENTO = "Pagamento";
+    String TITLE_APPBAR_RESUMO_COMPRA = "Resumo da Compra";
+}
